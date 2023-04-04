4d619b269b2290543e1928b1e3d68e4534912afe
insirenator <arsshakeeb149@protonmail.com>
2023-04-04 10:26:04 +0530

    Base Commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const amount = document.querySelector('.field');
+const tipSlider = document.querySelector('#tip-slider');
+const tipPerc = document.querySelector('#tip-perc');
+const tipAmount = document.querySelector('#tip');
+const totalAmount = document.querySelector('#total');
+
+const errorMsg = document.querySelector('#error');
+errorMsg.style.display = "none";
+
+amount.addEventListener('blur', () => {
+	if(!isNaN(parseFloat(amount.value))) {
+		tipSlider.disabled = false;
+		errorMsg.style.display = "none";
+	}
+	else {
+		errorMsg.style.display = "block";
+		tipSlider.disabled = true;
+	}
+});
+
+tipSlider.addEventListener('input', () => {
+	
+	const amt = parseFloat(amount.value);
+	const tip = parseInt(tipSlider.value);
+	tipPerc.textContent = `${tip}%`;
+
+	tipAmount.textContent = ((amt * tip) / 100).toFixed(2);
+
+	totalAmount.textContent = (amt +  (amt * tip) / 100).toFixed(2); 
+})
+
